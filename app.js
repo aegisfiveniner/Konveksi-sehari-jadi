28e948d23a2b5f91a0ac5a5e0602f39cf06821a8
aegisfiveniner <aegis.five.niner@gmail.com>
2022-03-24 21:48:08 +0700

    aegis59-feat:middleware dan logout

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const Controller = require('./controllers');
 const app = express()
 const port = 3000
+const session = require("express-session")
 
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+app.use(session({
+  secret: 'rahasia bah',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    sameSite: true
+  }
+}))
 
 
 
@@ -14,14 +24,25 @@ app.get('/', Controller.home)
 
 app.get("/register", Controller.regForm)
 app.post("/register", Controller.regPost)
-app.get("/login", Controller.loginForm)
-app.post("/login", Controller.loginCheck)
-
-
 
 //post register
 
+app.get("/login", Controller.loginForm)
+app.post("/login", Controller.loginCheck)
+
+app.use((req, res, next) => {
+  if(!req.session.userId) {
+    const error = "Login dulu"
+    res.redirect(`/login?error=${error}`)
+  } else {
+    next()
+  }
+})
+
+
 // app.get('/order/:id', Controller.order)
+
+app.get("/logout", Controller.getLogout)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
