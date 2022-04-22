f4476fa9af7557c03616403431a57eef16c6e743
aegisfiveniner <aegis.five.niner@gmail.com>
2022-04-22 16:08:34 +0700

    fix:app.js added if process != production and process.env.PORT

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  }
+
 const express = require('express');
 const Controller = require('./controllers');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const session = require("express-session")
 
 app.use(express.urlencoded({extended: true}));
